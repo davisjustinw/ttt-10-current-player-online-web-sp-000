f115f4f17c31bf15887861150b404c1d5bdf9739
Justin Davis <davisjustinw@gmail.com>
2019-01-06 05:28:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,7 @@
 def turn_count(board)
-  board.each_with_object(0) do |square, count|
-    #puts "#{square == 'X'}: #{square}"
+  count = 0
+  board.each do |square|
+    
     count = count + 1
     puts count
   end
