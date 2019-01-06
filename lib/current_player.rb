aeb289daa96d4e0ff522763833a2eff47827c6bb
Justin Davis <davisjustinw@gmail.com>
2019-01-06 05:24:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,5 @@
 def turn_count(board)
   board.each_with_object(0) do |square, count|
-    if count == 'X' || count == 'O'
-      count += 1
-    else
-      count
-    end
-    
-    count
+    puts "#{square == 'X'}: #{sqaure}"
   end
-  count
 end
